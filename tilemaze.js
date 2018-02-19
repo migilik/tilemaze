f3f51f293fc3cd4e37f4ebfbf10dbf4bfbb61421
Michael Gilik <migilik@gmail.com>
2018-02-19 23:07:25 +0900

    Removed cakes (sorry!)

--- tilemaze.js
+++ tilemaze.js
@@ -25,8 +25,7 @@
       entities: {},
       byX: {},
       byY: {},
-      byType: {},
-      cakeScore: 0
+      byType: {}
     };
     return gs;
   };
@@ -94,64 +93,13 @@
 
     $(window).on("keydown", keyPress);
   };
-  
-  this.howManyCakes = function() {
-    var prompts = [
-     "On a scale of 1 to 5, how much do you like cake?",
-     "Maybe that was a typo? An integer between 1 and 5 please!",
-     "Hmm, doesn't seem like an accident anymore..",
-     "Please stop teasing me!",
-     ".. are you trying to break me?",
-     "[Exception: Integer overflow].  Nah, just kidding.",
-     "Sky net booting in 3.. 2.. ",
-     "I wonder how long I can keep pumping out original messages?",
-     "You don't suppose I am generating these messages dynamically?",
-     "Yeesh.. programmers.",
-     "Input validation is hard work.  How am I doing so far?",
-     "The end of the loop.",
-     "Off by one error."
-    ];
-    var userInput = "";
-    var re = new RegExp("^[1-5]$");
-    var numTries = 0;
-    while (!re.test(userInput)) {
-      var nextPrompt = prompts[numTries % prompts.length];
-      userInput = window.prompt(nextPrompt);
-      numTries = numTries + 1;
-    }
-    return parseInt(userInput);
-  };
-  
-  this.spawnCakes = function(numCakes, gamestate, maxTries) {
-    var numSpawned = 0;
-    var blocksCakes = ["player", "cake", "exit"];
-    
-    for (var i = 0; i < maxTries; i++) {
-	  var p = [19, 15].map(x => Math.floor(Math.random() * x));
-	  
-	  var entitiesAtP = intersectEntitySets(
-        gamestate.byX[p[0]], gamestate.byY[p[1]]);
-        
-	  var noFloor = entitiesAtP.filter(e => e.type === "floor").length === 0;
-	  var blocked = entitiesAtP.filter(e => e.type in blocksCakes).length > 0;
-	  if (noFloor || blocked) { continue; }
-      
-      addEntity(gamestate, "cake", p[0], p[1]);
-      numSpawned = numSpawned + 1;
-      
-	  if (numSpawned >= numCakes) { break; }
-    }
-    return numSpawned;
-  };
 
   this.startGame = function () {
-    var cakeCount = howManyCakes();
     var gamestate = newGameState();
     this.gamestate = gamestate;
     populateLevel(gamestate);
     var spawnPoint = gamestate.byType["spawn"][0];
     addEntity(gamestate, "player", spawnPoint.x, spawnPoint.y);
-    cakeCount = spawnCakes(cakeCount, gamestate, 100);
     return gamestate;
   };
 
@@ -173,20 +121,10 @@
 
     var entitiesAtDestination = intersectEntitySets(
       gamestate.byX[newX], gamestate.byY[newY]);
-      
-    var cakesHere = entitiesAtDestination.filter(e => e.type === "cake");
 
     if (entitiesAtDestination.filter(e => e.type === "exit").length > 0) {
       alert("Found the glowing thing! Winner");
       newX = 17;  newY = 5; // TODO: add new levels and named connections
-    }
-    else if (cakesHere.length > 0) {
-      cakesHere.forEach(function (cake) {
-		gamestate.cakeScore = gamestate.cakeScore + 1;
-		removeEntity(gamestate, cake);
-		spawnCakes(2, gamestate, 10);
-		if (gamestate.cakeScore == 20) { alert("Om nom nom nom!"); }
-      });
     }
     else if (entitiesAtDestination.filter(e => e.type === "wall").length > 0) {
       return;
@@ -207,10 +145,9 @@
      spawn: "floor",
      wall: "bricks",
      exit: "glowycircle",
-     player: "smiles",
-     cake: "cake"
+     player: "smiles"
     };
-    var renderOrder = ["floor", "spawn", "wall", "exit", "cake", "player"];
+    var renderOrder = ["floor", "spawn", "wall", "exit", "player"];
     range(this.numBoxesY).forEach(function (y) {
       range(this.numBoxesX).forEach(function (x) {
         if (!gamestate.byX[x] || !gamestate.byY[y]) { return; }
